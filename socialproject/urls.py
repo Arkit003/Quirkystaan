9655dfe3402357744cf0cb95eda90308d6c28e55
Arkit <arkit312003@gmail.com>
2025-07-21 15:32:31 +0530

    Update urls.py to get the base url

--- socialproject/urls.py
+++ socialproject/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from users import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("users/",include("users.urls")),
     path("posts/",include("posts.urls")),
+    path("",views.user_login,name="index_page")
 ] 
 urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
